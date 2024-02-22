2cf242dfbe649f8b0ef1034ce87a8ee79bed9447
Zeynep Yeşilkaya <90097633+zyesilkaya@users.noreply.github.com>
2024-02-22 23:32:47 +0300

    some data changes

--- lib/views/user_profile_view.dart
+++ lib/views/user_profile_view.dart
@@ -62,7 +62,7 @@ class UserProfileView extends StatelessWidget {
                         const DecoratedBox(
                           decoration: BoxDecoration(
                             gradient: LinearGradient(
-                              begin: Alignment(0.0, 0.5),
+                              begin: Alignment(0.0, 1),
                               end: Alignment(0.0, 0.0),
                               colors: <Color>[
                                 Color(0x60000000),
@@ -83,8 +83,12 @@ class UserProfileView extends StatelessWidget {
                                   color:
                                       Colors.deepPurpleAccent.withOpacity(0.2),
                                 ),
-                                child: const Center(
-                                  child: Icon(Icons.person),
+                                child: Center(
+                                  child: Image.asset(
+                                    "assets/images/female_icon.png",
+                                    height: 260.h,
+                                    fit: BoxFit.fitHeight,
+                                  ),
                                 ),
                               ),
                             ),
